148f7b5a0bccf79d4098dddd3288c0f136ca5fc2
DiegoLibonati <diego.libonati1998@gmail.com>
2022-11-12 09:16:50 -0300

    new readme

--- README.md
+++ README.md
@@ -41,6 +41,138 @@ I made a web page that allows you to play hit the `mole` but in this case it is 
 
 ## Video
 
-
 https://user-images.githubusercontent.com/99032604/199616929-5f4a5738-43aa-4872-88f7-e336625e8b36.mp4
 
+## Documentation
+
+The `gridCreate()` function is in charge of creating the game board:
+
+```
+function gridCreate(gridValue){
+    for (i = 0; i < 25; i++){
+        const grid = document.createElement("div");
+        grid.setAttribute("id", i);
+        gridValue.append(grid);
+        gridArray.push(i);
+    }
+}
+```
+
+The `spawnRandom()` function is in charge of generating a spawnRandom of the rat in any cube of the board:
+
+```
+function spawnRandom(){
+    random = Math.floor(Math.random() * gridArray.length);
+    return random
+}
+```
+
+The `spawnEnemy()` function is in charge of generating the appearance of the rat on the Board:
+
+```
+function spawnEnemy(){
+
+    let numberRandom = spawnRandom();
+
+    const spawnRat = document.getElementById(`${numberRandom}`);
+
+    const rat = document.createElement("img");
+
+    ratArray.push(rat);
+
+    rat.setAttribute("src", "rata.png");
+    rat.setAttribute("class", "rat");
+
+    spawnRat.append(rat);
+
+    rat.addEventListener("click", (e)=>{
+        ratArray = [];
+        removeRat();
+        contador++
+        scoreUpdate();
+    });
+
+    if (ratArray.length >= 1){
+            ratArray = [];
+            setTimeout(removeRat, 2000);
+        } else {
+            console.log("Error.");
+        }
+
+}
+```
+
+The `removeRat()` function is in charge of removing the rat from the game board:
+
+```
+function removeRat(){
+    const getRat = document.querySelector(".rat");
+
+    try{
+        getRat.remove();
+    }catch(e){
+        console.log("Nada que remover")
+    }
+
+}
+```
+
+The `scoreUpdate()` function is in charge of updating the score each time we click on the rat:
+
+```
+function scoreUpdate(){
+    document.getElementById("score").innerHTML = contador;
+}
+```
+
+The `timeConfig()` function is in charge of subtracting time from the counter, the player will have one minute to make the most clicks on the rat:
+
+```
+function timeConfig(gridValue){
+
+    if(runTime <= 0){
+        clearInterval(clearTime);
+        clearInterval(clearGame);
+        document.getElementById("time").innerHTML = "The time is over";
+        document.querySelector(".contador_container p").innerHTML = `You score was ${contador}, Congrats. if you want, push in PLAY AGAIN`
+        gridValue.remove();
+
+    } else {
+        runTime--
+        document.getElementById("time").innerHTML = runTime;
+    }
+}
+```
+
+This gives the user the possibility once the time is up to play again without restarting the page, by clicking on `btnPlayAgain`:
+
+```
+btnPlayAgain.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  gridArray = [];
+  ratArray = [];
+  contador = 0;
+  runTime = 60;
+
+  const newGridDiv = document.createElement("div");
+  newGridDiv.setAttribute("class", "grid");
+
+  const parentDiv = document.querySelector(".section_container");
+
+  const beforeThis = document.querySelector(".btns_container");
+
+  parentDiv.insertBefore(newGridDiv, beforeThis);
+
+  gridCreate(newGridDiv);
+
+  document.querySelector(
+    ".contador_container p"
+  ).innerHTML = `Score: <span id="score">0</span>`;
+
+  clearGame = setInterval(spawnEnemy, 3000);
+  clearTime = setInterval(function () {
+    timeConfig(newGridDiv);
+  }, 1000);
+});
+```
--- codigo.js
+++ codigo.js
@@ -8,109 +8,108 @@ let runTime = 60;
 
 gridCreate(gridDisplay);
 
-function gridCreate(gridValue){
-    for (i = 0; i < 25; i++){
-        const grid = document.createElement("div");
-        grid.setAttribute("id", i);
-        gridValue.append(grid);
-        gridArray.push(i);
-    }
+function gridCreate(gridValue) {
+  for (i = 0; i < 25; i++) {
+    const grid = document.createElement("div");
+    grid.setAttribute("id", i);
+    gridValue.append(grid);
+    gridArray.push(i);
+  }
 }
 
-function spawnRandom(){
-    random = Math.floor(Math.random() * gridArray.length);
-    return random
+function spawnRandom() {
+  random = Math.floor(Math.random() * gridArray.length);
+  return random;
 }
 
-function spawnEnemy(){
+function spawnEnemy() {
+  let numberRandom = spawnRandom();
 
-    let numberRandom = spawnRandom();
+  const spawnRat = document.getElementById(`${numberRandom}`);
 
-    const spawnRat = document.getElementById(`${numberRandom}`);
+  const rat = document.createElement("img");
 
-    const rat = document.createElement("img");
+  ratArray.push(rat);
 
-    ratArray.push(rat);
+  rat.setAttribute("src", "rata.png");
+  rat.setAttribute("class", "rat");
 
-    rat.setAttribute("src", "rata.png");
-    rat.setAttribute("class", "rat");
+  spawnRat.append(rat);
 
-    spawnRat.append(rat);
+  rat.addEventListener("click", (e) => {
+    ratArray = [];
+    removeRat();
+    contador++;
+    scoreUpdate();
+  });
 
-    rat.addEventListener("click", (e)=>{
-        ratArray = [];
-        removeRat();
-        contador++
-        scoreUpdate();
-    });
-
-    if (ratArray.length >= 1){
-            ratArray = [];
-            setTimeout(removeRat, 2000);
-        } else {
-            console.log("Error.");
-        }
-
+  if (ratArray.length >= 1) {
+    ratArray = [];
+    setTimeout(removeRat, 2000);
+  } else {
+    console.log("Error.");
+  }
 }
 
-function removeRat(){
-    const getRat = document.querySelector(".rat");
+function removeRat() {
+  const getRat = document.querySelector(".rat");
 
-    try{
-        getRat.remove();
-    }catch(e){
-        console.log("Nada que remover")
-    }
-   
+  try {
+    getRat.remove();
+  } catch (e) {
+    console.log("Nada que remover");
+  }
 }
 
-function scoreUpdate(){
-    document.getElementById("score").innerHTML = contador;
+function scoreUpdate() {
+  document.getElementById("score").innerHTML = contador;
 }
 
-function timeConfig(gridValue){
-
-    if(runTime <= 0){
-        clearInterval(clearTime);
-        clearInterval(clearGame);
-        document.getElementById("time").innerHTML = "The time is over";
-        document.querySelector(".contador_container p").innerHTML = `You score was ${contador}, Congrats. if you want, push in PLAY AGAIN`
-        gridValue.remove();
-
-    } else {
-        runTime--
-        document.getElementById("time").innerHTML = runTime;
-    }
+function timeConfig(gridValue) {
+  if (runTime <= 0) {
+    clearInterval(clearTime);
+    clearInterval(clearGame);
+    document.getElementById("time").innerHTML = "The time is over";
+    document.querySelector(
+      ".contador_container p"
+    ).innerHTML = `You score was ${contador}, Congrats. if you want, push in PLAY AGAIN`;
+    gridValue.remove();
+  } else {
+    runTime--;
+    document.getElementById("time").innerHTML = runTime;
+  }
 }
 
 let clearGame = setInterval(spawnEnemy, 3000);
-let clearTime = setInterval(function() {timeConfig(gridDisplay)}, 1000);
+let clearTime = setInterval(function () {
+  timeConfig(gridDisplay);
+}, 1000);
 
-btnPlayAgain.addEventListener("click", (e)=>{
-    e.preventDefault()
+btnPlayAgain.addEventListener("click", (e) => {
+  e.preventDefault();
 
-    gridArray = [];
-    ratArray = [];
-    contador = 0;
-    runTime = 60;
+  gridArray = [];
+  ratArray = [];
+  contador = 0;
+  runTime = 60;
 
-    const newGridDiv = document.createElement("div");
-    newGridDiv.setAttribute("class", "grid");
-    
-    const parentDiv = document.querySelector(".section_container");
+  const newGridDiv = document.createElement("div");
+  newGridDiv.setAttribute("class", "grid");
 
-    const beforeThis = document.querySelector(".btns_container");
+  const parentDiv = document.querySelector(".section_container");
 
-    parentDiv.insertBefore(newGridDiv, beforeThis);
+  const beforeThis = document.querySelector(".btns_container");
 
-    gridCreate(newGridDiv);
+  parentDiv.insertBefore(newGridDiv, beforeThis);
 
-    document.querySelector(".contador_container p").innerHTML = `Score: <span id="score">0</span>`
+  gridCreate(newGridDiv);
 
+  document.querySelector(
+    ".contador_container p"
+  ).innerHTML = `Score: <span id="score">0</span>`;
 
-    clearGame = setInterval(spawnEnemy, 3000);
-    clearTime = setInterval(function() {timeConfig(newGridDiv)}, 1000);
-
+  clearGame = setInterval(spawnEnemy, 3000);
+  clearTime = setInterval(function () {
+    timeConfig(newGridDiv);
+  }, 1000);
 });
-
-
